cce84d8d5d979428236971a32309855418ce03f8
utkarshtipre2002 <utkarshtipre2002@gmail.com>
2024-03-05 01:15:37 +0530

    first commit

---
+++ src/components/Billing.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { apple, bill, google } from '../assets';
+import styles, { layout } from '../style'
+
+const Billing = () => {
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing"
+        className='w-[100%] h-[100%] relative z-[5]'/>
+
+        <div className='absolute z-[3] -left-1/2 top-0 w-[50%] rounded-full
+        white__gradient'/>
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] 
+        rounded-full pink__gradient"/>
+        
+
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your<br className='sm:block hidden' />
+          billing and invoicing.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Soluta provident deleniti aspernatur tempore.
+          Odio libero cumque labore! Molestiae expedita
+          voluptatum quas ea dolor! Earum repudiandae impedit
+          dignissimos, rem maiores voluptatem?
+        </p>
+        <div className='flex flex-row flex- wrap sm:mt-10 mt-6'>
+          <img src={apple} alt="apple_store" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+          <img src={google} alt="apple_store" className='w-[128px] h-[42px] object-contain cursor-pointer'/>
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Billing
